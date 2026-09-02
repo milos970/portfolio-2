b1146462b4568b19adeb88ad88209f86d385a5e2
Milos <dev980lop@gmail.com>
2026-09-02 21:37:15 +0200

    refactor: fluent switching pages

--- script.js
+++ script.js
@@ -23,3 +23,54 @@ toggle.addEventListener("change", () => {
     const lang = toggle.checked ? "en" : "sk";
     loadLanguage(lang);
 });
+
+
+const pageContent = document.getElementById("page-content");
+
+async function loadPage() {
+    let page = window.location.hash.substring(1);
+
+    // Defaultná stránka
+    if (!page) {
+        alert(5);
+        page = "home";
+    }
+
+    // Kontakt zostáva priamo v sidebare
+    if (page === "contact") {
+        document.getElementById("contact").scrollIntoView({
+            behavior: "smooth"
+        });
+        return;
+    }
+
+    try {
+        const response = await fetch(`pages/${page}.html`);
+
+        if (!response.ok) {
+            throw new Error("Page not found");
+        }
+
+        pageContent.innerHTML = await response.text();
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    } catch (error) {
+        pageContent.innerHTML = `
+            <section>
+                <header class="main">
+                    <h1>404</h1>
+                </header>
+
+                <p>Stránka neexistuje.</p>
+            </section>
+        `;
+    }
+}
+
+window.addEventListener("hashchange", loadPage);
+
+loadPage();
